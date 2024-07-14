f5e0a37df9c917ef3716b7ed1ff0a8954720953c
shekhu <s43khu@gmail.com>
2024-07-14 22:37:28 +0530

    added confetti

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import toast from "react-hot-toast";
-
+import confetti from "canvas-confetti";
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
@@ -26,6 +26,35 @@ const Contact = () => {
       [name]: value,
     });
   };
+
+  const confettiFire=()=>{
+    const duration = 3 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+ 
+    const randomInRange = (min, max) =>
+      Math.random() * (max - min) + min;
+ 
+    const interval = window.setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+ 
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+ 
+      const particleCount = 50 * (timeLeft / duration);
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    }, 250);
+  }
 
   const validate = () => {
     let tempErrors = {};
@@ -53,6 +82,7 @@ const Contact = () => {
       .then(
         (result) => {
           setLoading(false);
+          confettiFire();
           toast.success("Thank you. I will get back to you as soon as possible.");
           setForm({
             from_name: "",
